6f13ae88d17f5f6f1dcb0774f8a382a537d81cf5
allen54a0 <allen54a0@gmail.com>
2018-09-12 21:57:50 +0800

    Create FLORA_ESP32_SINGLE.ino

---
+++ FLORA_ESP32_SINGLE/FLORA_ESP32_SINGLE.ino
@@ -0,0 +1,235 @@
+/**
+   CODE from https://github.com/sidddy/flora
+   
+   A BLE client for the Xiaomi Mi Plant Sensor, pushing measurements to an MQTT server.
+   
+   MIT License
+   Copyright (c) 2017 Sven Henkel
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+#include "BLEDevice.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#include "config.h"
+
+RTC_DATA_ATTR int bootCount = 0;
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("00001204-0000-1000-8000-00805f9b34fb");
+
+// The characteristic of the remote service we are interested in.
+static BLEUUID uuid_version_battery("00001a02-0000-1000-8000-00805f9b34fb");
+static BLEUUID uuid_sensor_data("00001a01-0000-1000-8000-00805f9b34fb");
+static BLEUUID uuid_write_mode("00001a00-0000-1000-8000-00805f9b34fb");
+
+static BLEAddress floraAddress(FLORA_ADDR);
+
+static int doConnect = 0;
+static boolean connected = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+bool getSensorData(BLEAddress pAddress, bool getBattery) {
+  Serial.print("Forming a connection to Flora device at ");
+  Serial.println(pAddress.toString().c_str());
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+
+  // Connect to the remove BLE Server.
+  if (!pClient->connect(pAddress)) {
+     Serial.println(" -NGNGNGNGNG  Connected to Flora");
+      return false;
+  }
+  Serial.println(" - Connected to Flora");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_write_mode);
+  uint8_t buf[2] = {0xA0, 0x1F};
+  pRemoteCharacteristic->writeValue(buf, 2, true);
+
+  delay(500);
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_sensor_data);
+  Serial.println(pRemoteService->toString().c_str());
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(uuid_sensor_data.toString().c_str());
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+
+  // Read the value of the characteristic.
+  std::string value = pRemoteCharacteristic->readValue();
+  Serial.print("The characteristic value was: ");
+  const char *val = value.c_str();
+
+  Serial.print("Hex: ");
+  for (int i = 0; i < 16; i++) {
+    Serial.print((int)val[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+
+  float temp = (val[0] + val[1] * 256) / ((float)10.0);
+  int moisture = val[7];
+  int light = val[3] + val[4] * 256;
+  int conductivity = val[8] + val[9] * 256;
+  
+  char buffer[64];
+  
+  Serial.print("Temperature: ");
+  Serial.println(temp);
+  snprintf(buffer, 64, "%f", temp);
+  client.publish(MQTT_TEMPERATURE, buffer);
+  
+
+  Serial.print("Moisture: ");
+  Serial.println(moisture);
+  snprintf(buffer, 64, "%d", moisture);
+  client.publish(MQTT_MOISTURE, buffer);
+
+  Serial.print("Light: ");
+  Serial.println(light);
+  snprintf(buffer, 64, "%d", light);
+  client.publish(MQTT_LIGHT, buffer);
+
+  Serial.print("Conductivity: ");
+  Serial.println(conductivity);
+  snprintf(buffer, 64, "%d", conductivity);
+  client.publish(MQTT_CONDUCTIVITY, buffer);
+
+  if (getBattery) {
+    Serial.println("Trying to retrieve battery level...");
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_version_battery);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(uuid_sensor_data.toString().c_str()); 
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic...
+    value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    const char *val2 = value.c_str();
+    Serial.print("Hex: ");
+    for (int i = 0; i < 16; i++) {
+      Serial.print((int)val2[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
+
+    int battery = val2[0];
+    Serial.print("Battery: ");
+    Serial.println(battery);
+    snprintf(buffer, 64, "%d", battery);
+    client.publish(MQTT_BATTERY, buffer);
+      Serial.print(MQTT_BATTERY );
+       Serial.print(buffer );
+  }
+
+  pClient->disconnect();
+}
+
+void taskDeepSleep( void * parameter )
+{
+  delay(SLEEP_WAIT * 1000);
+  esp_sleep_enable_timer_wakeup(SLEEP_DURATION * 1000000ll);
+  Serial.println("Going to sleep now.");
+  esp_deep_sleep_start();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  xTaskCreate(      taskDeepSleep,          /* Task function. */
+                    "TaskDeepSleep",        /* String with name of task. */
+                    10000,                  /* Stack size in words. */
+                    NULL,                   /* Parameter passed as input of the task */
+                    1,                      /* Priority of the task. */
+                    NULL);                  /* Task handle. */
+  
+  
+  Serial.println("Starting Flora client...");
+  BLEDevice::init("");
+
+  Serial.println("Connecting WiFi...");
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  client.setServer(mqtt_server, 1883);
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("floraClient","allen54a0","478d23a8118842f1a8bf4741a648463b")) {   //for Ada Fruit IO
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+
+  delay(1000);
+
+//if(  getSensorData(floraAddress, ((bootCount % BATTERY_INTERVAL) == 0))){
+if(  getSensorData(floraAddress, true)){
+
+  
+}else{
+      
+       Serial.println("Try Again");
+       delay(500);
+       getSensorData (floraAddress, true);
+}
+
+
+
+  bootCount++;
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+  delay(10000);
+} // End of loop
